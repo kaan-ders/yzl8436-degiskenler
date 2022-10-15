ea213f02edda58e9dd524aa9e93fb13be40c6e6b
Kaan Arslan <115875320+kaan-ders@users.noreply.github.com>
2022-10-15 14:16:08 +0300

    initial-commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace yzl8436_degiskenler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*byte deneme = 255;
+            int sayi;
+            int sayi_2;
+            string isim3;
+            string musteriIsmi = "sfsdfsdfsdfsdfsd0";
+            char karakter = 'a';
+
+            int mukemmel = 0;
+
+            bool tamamlandiMi = true; //false
+
+            mukemmel = mukemmel + 1;
+            musteriIsmi = musteriIsmi + 1;
+
+            DateTime tarih = DateTime.Now;
+
+            //int sayi3 = 0;
+            var sayi3 = 0;
+            //sayi3 = "mehmet";
+
+            int s1, s2 = 0, s3;*/
+            /*int s1;
+            int s2;
+            int s3;*/
+
+            //merhaba nasılsınız? - tek satırlı açıklama satırı
+
+            /*
+              çok
+                satırlı açıklama
+                satırları
+            */
+
+
+            //int sayi1 = 32, sayi2 = 45;
+            //int sonuc1 = 32 - 45;
+            //int sonuc1 = sayi1 - sayi2;
+            //int sonuc1 = sayi1 + sayi2;
+            //int sonuc1 = sayi1 * sayi2;
+            //float sonuc1 = sayi2 / sayi1;
+            //float sonuc1 = sayi2 % 7;
+
+            //Console.WriteLine(sonuc1);
+
+            //Console.Write("Merhaba" + tarih);
+            //Console.WriteLine(2);
+
+            int okunanSayi = 0;
+
+            Console.Write("Lütfen bir sayı giriniz: ");
+            okunanSayi = Convert.ToInt32(Console.ReadLine());
+            okunanSayi = okunanSayi * 2;
+
+            Console.WriteLine("İşleminizin sonucu: " + okunanSayi);
+
+            //string isim = "ahmet'in yaşı: " + 43; //43
+            //Console.WriteLine(isim);
+        }
+    }
+}
